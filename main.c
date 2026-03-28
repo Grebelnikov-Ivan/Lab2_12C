10289b3a8cedc67af83070419bd7ecfc48a63ee3
Grebelnikov_Ivan <grivmih@gmail.com>
2026-03-28 22:05:29 +0700

    cleanMatrix
    and int**->char**

--- main.c
+++ main.c
@@ -2,41 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-int** createMatrix(int rows, int cols);
-int fillingMatrix(int** matrix);
-int checkingCorrectMatrix(int** matrix);
-int transpositionMatrix(int** matrix);
-int printMatrix(int** matrix);
-int cleanMatrix(int** matrix);
+char** createMatrix(int rows, int cols);
+int fillingMatrix(char** matrix);
+int checkingCorrectMatrix(char** matrix);
+int transpositionMatrix(char** matrix);
+int printMatrix(char** matrix);
+int cleanMatrix(char** matrix);
 
 
 int main() {
     int rows = 2, cols = 3;
-    int** matrix = NULL;
+    char** matrix = NULL;
     matrix = createMatrix(rows, cols);
 
     if (matrix != NULL) {
         // используем
 
         // освобождаем
-        for (int i = 0; i < rows; i++) {
-            free(matrix[i]);
-        }
-        free(matrix);
+        cleanMatrix(matrix);
     }
     return 0;
 }
 
 
-int** createMatrix(int rows, int cols){
-    int** matrix = (int**)calloc(rows, sizeof(int*));
+char** createMatrix(int rows, int cols){
+    char** matrix = (char**)calloc(rows, sizeof(char*));
 
     if (matrix == NULL) {
         return NULL;
     }
 
     for (int i = 0; i < rows; i++) {
-        matrix[i] = (int*)calloc(cols, sizeof(int));
+        matrix[i] = (char*)calloc(cols + 1, sizeof(char));
+        matrix[i][cols] = '\n';
+
         if (matrix[i] == NULL) {
             for (int j = 0; j < i; j++) {
                 free(matrix[j]);
@@ -45,32 +44,37 @@ int** createMatrix(int rows, int cols){
             return NULL;
         }
     }
+    matrix[rows] = NULL;
 
     return matrix;
 }
 
 
-int fillingMatrix(int** matrix){
+int fillingMatrix(char** matrix){
     return 0;
 }
 
 
-int checkingCorrectMatrix(int** matrix){
+int checkingCorrectMatrix(char** matrix){
     return 0;
 }
 
 
-int transpositionMatrix(int** matrix){
+int transpositionMatrix(char** matrix){
     return 0;
 }
 
 
-int printMatrix(int** matrix){
+int printMatrix(char** matrix){
     return 0;
 }
 
 
-int cleanMatrix(int** matrix){
+int cleanMatrix(char** matrix){
+    for (int i = 0; matrix[i] != NULL; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
     return 0;
 }
 
