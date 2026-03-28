f95d7081c5c5c4ff3609bc8d2cc62b5e83fbf8e0
Grebelnikov_Ivan <grivmih@gmail.com>
2026-03-28 21:28:40 +0700

    make f createMatrix

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-int createMatrix(int rows, int cols);
+int** createMatrix(int rows, int cols);
 int fillingMatrix(int** matrix);
 int checkingCorrectMatrix(int** matrix);
 int transpositionMatrix(int** matrix);
@@ -11,13 +11,42 @@ int cleanMatrix(int** matrix);
 
 
 int main() {
+    int rows = 2, cols = 3;
+    int** matrix = NULL;
+    matrix = createMatrix(rows, cols);
 
+    if (matrix != NULL) {
+        // используем
+
+        // освобождаем
+        for (int i = 0; i < rows; i++) {
+            free(matrix[i]);
+        }
+        free(matrix);
+    }
     return 0;
 }
 
 
-int createMatrix(int rows, int cols){
-    return 0;
+int** createMatrix(int rows, int cols){
+    int** matrix = (int**)calloc(rows, sizeof(int*));
+
+    if (matrix == NULL) {
+        return NULL;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = (int*)calloc(cols, sizeof(int));
+        if (matrix[i] == NULL) {
+            for (int j = 0; j < i; j++) {
+                free(matrix[j]);
+            }
+            free(matrix);
+            return NULL;
+        }
+    }
+
+    return matrix;
 }
 
 
