33cac67a16139de2cbcfb83f72cbdffcc4284608
Grebelnikov_Ivan <grivmih@gmail.com>
2026-03-28 20:42:24 +0700

    created the program framework

--- main.c
+++ main.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int createMatrix(int rows, int cols);
+int fillingMatrix(int** matrix);
+int checkingCorrectMatrix(int** matrix);
+int transpositionMatrix(int** matrix);
+int printMatrix(int** matrix);
+int cleanMatrix(int** matrix);
+
 
 int main() {
 
     return 0;
 }
+
+
+int createMatrix(int rows, int cols){
+    return 0;
+}
+
+
+int fillingMatrix(int** matrix){
+    return 0;
+}
+
+
+int checkingCorrectMatrix(int** matrix){
+    return 0;
+}
+
+
+int transpositionMatrix(int** matrix){
+    return 0;
+}
+
+
+int printMatrix(int** matrix){
+    return 0;
+}
+
+
+int cleanMatrix(int** matrix){
+    return 0;
+}
+
+
+
