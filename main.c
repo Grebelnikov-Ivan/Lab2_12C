8cfb7fd4520e1f70ccb7747b0d47af8b0596ffb3
Grebelnikov_Ivan <grivmih@gmail.com>
2026-03-29 13:51:49 +0700

    fixed various bugs

--- main.c
+++ main.c
@@ -7,55 +7,80 @@ int fillingMatrix(int** matrix, int rows, int cols);
 int checkingCorrectMatrix(int** matrix, int rows, int cols);
 int transpositionMatrix(int** matrix, int** matrix2, int rows, int cols);
 int printMatrix(int** matrix, int rows, int cols);
-int cleanMatrix(int** matrix, int rows);
-
+void cleanMatrix(int** matrix, int rows);
 
 int main() {
     int rows = 3, cols = 4;
     int** matrix = NULL;
     int** matrix2 = NULL;
+
     matrix = createMatrix(rows, cols);
-    matrix2 = createMatrix(cols, rows);
-
-    if (matrix != NULL && matrix2 != NULL) {
-        // используем
-        fillingMatrix(matrix, rows, cols);
-
-        checkingCorrectMatrix(matrix, rows, cols);
-        checkingCorrectMatrix(matrix2, rows, cols);
-
-        transpositionMatrix(matrix, matrix2, rows, cols);
-
-        printMatrix(matrix, rows, cols);
-        printMatrix(matrix2, cols, rows);
-
-        // освобождаем
-        cleanMatrix(matrix, rows);
-        matrix = NULL;
-
-        cleanMatrix(matrix2, cols);
-        matrix2 = NULL;
-    }
-    else {
-        if (matrix != NULL) {
-            cleanMatrix(matrix, rows);
-            matrix = NULL;
-        }
-        if (matrix2 != NULL) {
-            cleanMatrix(matrix2, cols);
-            matrix2 = NULL;
-        }
+    if (matrix == NULL) {
+        printf("error1");
         return 1;
     }
+
+    matrix2 = createMatrix(cols, rows);
+    if (matrix2 == NULL) {
+        printf("error2");
+        cleanMatrix(matrix, rows);
+        return 1;
+    }
+
+    if (fillingMatrix(matrix, rows, cols) != 0) {
+        printf("error3");
+        cleanMatrix(matrix, rows);
+        cleanMatrix(matrix2, cols);
+        return 1;
+    }
+
+    if (checkingCorrectMatrix(matrix, rows, cols) != 0) {
+        printf("error4");
+        cleanMatrix(matrix, rows);
+        cleanMatrix(matrix2, cols);
+        return 1;
+    }
+
+    if (checkingCorrectMatrix(matrix2, cols, rows) != 0) {
+        printf("error5");
+        cleanMatrix(matrix, rows);
+        cleanMatrix(matrix2, cols);
+        return 1;
+    }
+
+    if (transpositionMatrix(matrix, matrix2, rows, cols) != 0) {
+        printf("error6");
+        cleanMatrix(matrix, rows);
+        cleanMatrix(matrix2, cols);
+        return 1;
+    }
+
+    // Выводим исходную матрицу
+    if (printMatrix(matrix, rows, cols) != 0) {
+        printf("error7");
+        cleanMatrix(matrix, rows);
+        cleanMatrix(matrix2, cols);
+        return 1;
+    }
+
+    if (printMatrix(matrix2, cols, rows) != 0) {
+        printf("error8");
+        cleanMatrix(matrix, rows);
+        cleanMatrix(matrix2, cols);
+        return 1;
+    }
+
+    cleanMatrix(matrix, rows);
+    cleanMatrix(matrix2, cols);
+
     return 0;
 }
 
-
-int** createMatrix(int rows, int cols){
+int** createMatrix(int rows, int cols) {
     if (rows <= 0 || cols <= 0)
         return NULL;
-    int** matrix = (int**)calloc(rows, sizeof(int*));
 
+    int** matrix = (int**)calloc(rows, sizeof(int*));
     if (matrix == NULL) {
         return NULL;
     }
@@ -70,12 +95,10 @@ int** createMatrix(int rows, int cols){
             return NULL;
         }
     }
-
     return matrix;
 }
 
-
-int fillingMatrix(int** matrix, int rows, int cols){
+int fillingMatrix(int** matrix, int rows, int cols) {
     if (matrix == NULL || rows <= 0 || cols <= 0)
         return 1;
 
@@ -89,39 +112,35 @@ int fillingMatrix(int** matrix, int rows, int cols){
     return 0;
 }
 
-
-int checkingCorrectMatrix(int** matrix, int rows, int cols){
+int checkingCorrectMatrix(int** matrix, int rows, int cols) {
     if (matrix == NULL) {
         return 1;
     }
 
     if (rows <= 0 || cols <= 0) {
-        return 1;
+        return 2;
     }
 
     int f = 0;
     for (int i = 0; i < rows; i++) {
-        if (matrix[i] == 0) {
+        if (matrix[i] == NULL) {
             f++;
         }
     }
     if (f == rows)
-        return 1;
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            int value = matrix[i][j];
-        }
-    }
+        return 3;
 
     return 0;
 }
 
-
-int transpositionMatrix(int** matrix, int** matrix2, int rows, int cols){
-    if (matrix == NULL || matrix2 == NULL || rows <= 0 || cols <= 0)
+int transpositionMatrix(int** matrix, int** matrix2, int rows, int cols) {
+    if (rows <= 0 || cols <= 0)
         return 1;
 
+    if (matrix == NULL || matrix2 == NULL)
+        return 2;
+
+    // транспонирование
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             matrix2[j][i] = matrix[i][j];
@@ -130,12 +149,14 @@ int transpositionMatrix(int** matrix, int** matrix2, int rows, int cols){
     return 0;
 }
 
-
-int printMatrix(int** matrix, int rows, int cols){
-    if (matrix == NULL|| rows <= 0 || cols <= 0)
+int printMatrix(int** matrix, int rows, int cols) {
+    if (matrix == NULL || rows <= 0 || cols <= 0)
         return 1;
 
     for (int i = 0; i < rows; i++) {
+        if (matrix[i] == NULL)
+            return 2;
+
         for (int j = 0; j < cols; j++) {
             printf("%d\t", matrix[i][j]);
         }
@@ -145,18 +166,20 @@ int printMatrix(int** matrix, int rows, int cols){
     return 0;
 }
 
+void cleanMatrix(int** matrix, int rows) {
+    if (matrix == NULL)
+        return ;
 
-int cleanMatrix(int** matrix, int rows){
-    if (matrix == NULL || rows <= 0)
-        return 1;
+    if (rows <= 0) {
+        free(matrix);
+        return ;
+    }
 
     for (int i = 0; i < rows; i++) {
-        free(matrix[i]);
+        if (matrix[i] != NULL) {
+            free(matrix[i]);
+        }
     }
+
     free(matrix);
-
-    return 0;
 }
-
-
-
